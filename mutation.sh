92abd196d66dd4cbc6ee969f2d508993e6b6f317
Eric Frankel <eric-frankel@cox.net>
2018-08-17 14:10:45 -0700

    finished mutation.sh and added python requirements

--- mutation.sh
+++ mutation.sh
@@ -16,9 +16,37 @@ function run_mut() {
         #TODO: add in the part that adds the header to the results file
     fi
 
-    # Checking if repaired_pdb exists IN CONTEXT OF DNM1; if not, throw error
-    if [ ! -f "./
+    # Checking if repaired_pdb exists IN CONTEXT OF DNM1; if not, terminate
+    if [ ! -f "./3snh_Repair.pdb" ]; then
+        echo "No repaired PDB file found; terminating";
+        return;
+    fi
 
     # Execute foldx script portion
-    ~/Desktop/foldx/foldx --command=BuildModel --pdb=3snh_Repair.pdb --mutant-file=mutant_file.txt --ionStrength=0.05 --pH=7 --water=CRYSTAL --vdwDesign=2 --pdbHydrogens=false --numberOfRuns=10
+    ~/Desktop/foldx/foldx --command=BuildModel --pdb=3snh_Repair.pdb --mutant-file=mutant_file.txt --ionStrength=0.05 --pH=7 --water=CRYSTAL --vdwDesign=2 --pdbHydrogens=false --numberOfRuns=10;
+
+    # Handle all files that get generated, removing the useless ones
+    rm WT*;
+    rm 3snh_Repair_*;
+    rm PdbList*;
+    rm Dif_3snh*;
+    mv Average_3snh_Repair.fxout Average_3snh_Repair.txt
+
+    # Defining constants for future operations
+    TAB=$'\t'
+    NLINE=$'\n'
+
+    # Adds temporary text file; parses through the text file to add additional column
+    # TODO: This process feels clunky af but I don't want to mess with it. Play around and check if it can be more efficient. Like could we just do it in a dataframe and leave as is?
+    cp Average_3snh_Repair.txt temp.txt
+    sed -i".bak" '1,9d' temp.txt
+	sed -e '$s/$/'"${TAB}"''"$1"'/' temp.txt >> temp.txt
+	sed -i".bak" '1d' temp.txt
+	for line in temp.txt
+	do
+		echo -e '\n'
+		cat $line
+	done >> results.txt
+	> temp.txt
+	rm Average_3snh_Repair.txt
 }

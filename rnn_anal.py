dd2c984428f382690469cd002d84dbc5b7e214a2
Eric Frankel <eric-frankel@cox.net>
2018-08-19 23:22:35 -0700

    adding better structure for results file

--- rnn_anal.py
+++ rnn_anal.py
@@ -4,14 +4,21 @@ import pandas as pd
 import subprocess
 
 
-def run_rnn(training_path):
-    mutations = pd.read_csv(training_path)
+def create_results_file(missense_path):
+    mutations = pd.read_csv(missense_path)
     missenses = mutations['Protein Consequence']
 
     for mutation in missenses:
         mut = mutation[2:]
         new_mut = parse_aa(mut[0:3]) + mut[3:-3] + parse_aa(mut[-3:])
         subprocess.call("./mutation.sh && run_mut() " + new_mut, shell=True) # TODO: add function call from the mutation script
+    return "Finished creating results file"
+
+
+def combine_mut_dataframes(missense_path, results_path="./results.txt"):
+    mutations = pd.read_csv(missense_path)
+    results = pd.read_csv(results_path)
+    mutations
 
 
 def parse_aa(mutation):
